a5947a1b074c50e23792b2455316a7950df85d8c
ajosan25 <97420031+ajosan25@users.noreply.github.com>
2024-05-02 11:20:28 -0700

    Main game loop

---
+++ main.py
@@ -0,0 +1,20 @@
+def load_game():
+    # If they have preloaded gamestate, load that here
+    pass
+
+def choose_archetype():
+    pass
+
+def buy_supplies():
+    pass
+
+# save player state in variables here
+
+def game_loop():
+    # month passes
+    # action
+    # Randomly pre-scheduled event
+    pass
+
+def finish():
+    pass
