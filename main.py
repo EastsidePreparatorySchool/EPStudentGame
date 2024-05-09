af867d82e96e2d52103d757f637ac7f9e6730d44
ajosan25 <97420031+ajosan25@users.noreply.github.com>
2024-05-09 11:13:07 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ def load_game():
     # If they have preloaded gamestate, load that here
     pass
 
+def save_game():
+    pass
+
 def choose_archetype():
     pass
 
@@ -9,8 +12,19 @@ def buy_supplies():
     pass
 
 # save player state in variables here
+month = 0
+year = "Freshman"
 
+def check_quit():
+    quit = input("Would you like to save and quit? (Y/N)")
+    if (quit.lower() == "Y"):
+        save_game()
+        return True
+    else:
+        return False
 def game_loop():
+    if (check_quit() is not True):
+        print("Month: " + month, "Year " + year)
     # month passes
     # action
     # Randomly pre-scheduled event
