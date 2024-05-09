bbac962c4ce71ca2096cb6ac5c18a9c685cabbe3
ajosan25 <97420031+ajosan25@users.noreply.github.com>
2024-05-09 11:30:25 -0700

    Save/load framework

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+class GameState():
+    def __init__(self, month, year):
+        self.month = month
+        self.year = year
+
 def load_game():
     # If they have preloaded gamestate, load that here
-    pass
+    load = input("Would you like to load a previous game? (Y/N) ")
+    if (load.lower() == "y"):
+        filepath = input("Enter the file path of the save file: ")
+    else:
+        return GameState(0, "Freshman")
 
-def save_game():
-    pass
+def save_game(g):
+    filepath = input("Enter the location where you would like to save: ")
 
 def choose_archetype():
     pass
@@ -11,24 +20,38 @@ def choose_archetype():
 def buy_supplies():
     pass
 
-# save player state in variables here
-month = 0
-year = "Freshman"
-
 def check_quit():
-    quit = input("Would you like to save and quit? (Y/N)")
-    if (quit.lower() == "Y"):
-        save_game()
-        return True
-    else:
-        return False
-def game_loop():
-    if (check_quit() is not True):
-        print("Month: " + month, "Year " + year)
+    quit = input("Would you like to save and quit? (Y/N) ")
+    return quit.lower() == "y"
+
+def game_loop(g):
+    if (check_quit()):
+        save_game(g)
+        return
+    
+    print("Month: " + str(g.month), "\nYear: " + g.year)
+    if(g.month == 0 and g.year == "Freshman"):
+        choose_archetype()
+    
     # month passes
-    # action
+    g.month += 1
+    g.month %= 12
+    
+    if (g.year == "Freshman"):
+        g.year = "Sophomore"
+    elif (g.year == "Sophomore"):
+        g.year = "Junior"
+    elif (g.year == "Junior"):
+        g.year = "senior"
+    game_loop(g)
+    
+    # action phase - recursive
+    
     # Randomly pre-scheduled event
-    pass
 
 def finish():
-    pass
+    pass    
+
+# save player state in variables here
+g = load_game()
+game_loop(g)
